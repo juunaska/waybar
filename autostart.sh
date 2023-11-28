8dd7a65c8ca65cc9242f80c2c9c8dc3b722409b9
juunaska <118882741+juunaska@users.noreply.github.com>
2023-11-28 20:23:40 +0200

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,9 @@
+
+# AUTOSTART HYPRLAND
+
+swww init && sleep 1 &
+swww clear 31222c &
+
+
+
+
---
+++ specialworkspace.sh
@@ -0,0 +1,13 @@
+
+# SPECIALWORKSPACE
+
+
+hyprctl dispatch togglespecialworkspace special &
+
+/home/test/./pyradio.sh &
+
+com.simplenote.Simplenote &
+
+gnome-calendar &
+
+
